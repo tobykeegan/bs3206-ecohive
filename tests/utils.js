a19a8517997c352ef8b5ce013b7ecd37f66471b6
Toby Keegan <Toby.Keegan@ibm.com>
2024-01-25 16:11:09 +0000

    Utility function to skip test if no impl

---
+++ tests/utils.js
@@ -0,0 +1,12 @@
+import { test } from '@playwright/test';
+
+
+/**
+ * Skips the test if the functionality is not implemented
+ * @param {boolean} implemented 
+ */
+export const implemented = (implemented) => {
+	if(implemented === false && process.env.NODE_ENV != 'production'){
+		test.skip(true, 'Not implemented')
+	}
+}
