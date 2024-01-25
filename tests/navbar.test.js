636d2e1aa1c4a6763140ab3546eed2fd98dd3e04
Toby Keegan <Toby.Keegan@ibm.com>
2024-01-25 16:10:54 +0000

    Sample test for Navbar

---
+++ tests/navbar.test.js
@@ -0,0 +1,47 @@
+import { expect, test } from '@playwright/test';
+import { implemented } from './utils'
+/* 
+	Test Navbar visibility.
+	This group of tests checks each page listed under 
+
+*/
+
+
+test.describe('Navbar visibility', () => {
+	const pages = {
+		visible: [
+			"/home",
+			"/settings",
+			"/events/feed",
+			"/events/myevents"
+		],
+		hidden: [
+			"/",		// shouldn't ever be here!
+			"/login",
+			"/register"
+		]
+	}
+	pages.visible.forEach(url => {
+		test(`Visible on '${url}' page`, async ({ page }) => {
+			await page.goto(url);
+			await expect(
+				page.locator('nav', { name: 'navbar' })
+			).toBeVisible();
+		});
+		
+	});
+	
+	pages.hidden.forEach(url => {
+
+		test(`Not visible on '${url}' page`, async ({ page }) => {
+
+			implemented(false);
+
+			await page.goto(url);
+			await expect(
+				page.locator('nav', { name: 'navbar' })
+			).not.toBeVisible();
+		});
+		
+	});
+})
