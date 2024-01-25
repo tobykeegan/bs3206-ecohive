ca14345a5dc9e83780b2d40ad61a44877796ead6
Toby Keegan <Toby.Keegan@ibm.com>
2024-01-25 16:10:22 +0000

    Few more links in navbar

--- src/app/ui/Navbar.jsx
+++ src/app/ui/Navbar.jsx
@@ -2,10 +2,12 @@ import Grid from "@mui/joy/Grid";
 
 export default function Navbar() {
   return (
-    <nav>
+    <nav id="navbar">
       <h1>EcoHive Navbar</h1>
       <a href="/">Home</a>
       <a href="/settings">Settings</a>
+      <a href="/events/feed">Events</a>
+      <a href="/events/feed">My Events</a>
     </nav>
   );
 }
