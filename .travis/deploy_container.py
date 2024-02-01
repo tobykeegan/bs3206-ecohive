371ed8696315a36d8a70fae07ee1207b5aec4a77
Toby Keegan <Toby.Keegan@ibm.com>
2024-02-01 23:09:48 +0000

    Significant enhancements to deploy logic

--- .travis/deploy_container.py
+++ .travis/deploy_container.py
@@ -3,20 +3,27 @@
 import argparse
 import logging
 import time
-import socket, errno
+import sys
+import socket
+import errno
+import subprocess
 from subprocess import check_output
 
 class DeployHelper:
+    '''
+    A utility class that enables a new container image to be deployed
+    by using its deploy() function.
+    '''
     
     def __init__(self, image, branch):
-
+        
+        # basic logging configuration
         logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', 
                             level=logging.DEBUG, 
                             handlers=[
                                 logging.FileHandler('deployments.log'),
                                 logging.StreamHandler()
                             ])
-
         self.log = logging.getLogger(__name__)
 
         # check the args
@@ -36,31 +43,65 @@ class DeployHelper:
         
         
     def deploy(self):
-        self.log.info("======= START DEPLOYMENT =======")
-        self.docker_ps()
-        self.log.info("Setting up a new instance of %s from branch %s", self.image, self.branch)
+        '''
+        Runs the steps required to deploy a new container image 
+        to the machine.
+            1. Stops any containers running this image
+            2. Removes the container from Docker's cache so the name can be used again
+            3. Locates a port suitable for the container:
+                3000        -> ecohive-ui:latest    (reserved)
+                3001-3999   -> ecohive-ui:<branch>
+                4000        -> ecohive-api:latest   (reserved)
+                4001-4999   -> ecohive-api:<branch>
+            4. Starts the new container and performs a crude check to verify it is up.
+        '''
         
         container_name = f"{self.image}-{self.branch}"
+        self.log.info("======= START DEPLOYMENT of '%s' =======", container_name)
         
-        self.log.debug("Container name will be '%s'", container_name)
+        self.docker_ps()
+        
+        
+        self.log.info("Setting up a new instance of %s from branch %s", self.image, self.branch)
 
-        try:
-            self.run(f"docker stop {container_name}")
-        except:
-            self.log.warning("Container %s was not running.", container_name)
+        if self.container_is_running(container_name):
+            self.log.info("'%s' is running, attempting to stop")
+            self.run(f"docker kill {container_name}")
         
+        for _ in range(5):
+            if not self.container_is_running(container_name):
+                self.log.info("Container '%s' successfully stopped", container_name)
+                break
+            else:
+                self.fail("Container '%s' could not be stopped", container_name)
+                
+                
         try:
             self.run(f"docker container rm {container_name}")
-        except:
-            self.log.warning(f"No dangling container '{container_name}' to remove")
+            self.log.info("Old container successfully removed")
+        except subprocess.CalledProcessError as err:
+            if err.output.decode() == f'Error response from daemon: No such container: {container_name}\n':
+                self.log.info("No dangling container '%s' to remove", container_name)
+            else:
+                self.log.critical(err.output)
+                self.fail(f"Shell command 'docker container rm {container_name} failed. See above output")
 
         port = self.get_free_port()
-        self.log.info("Starting new container:")
-        container = self.run(f"docker run -d --name {container_name} -p {port}:3000 {self.image}:{self.branch}")
-        self.log.info("Container ID: " + container)
-        WAIT_TIME = 20
+        
+        
+        self.log.info("Starting new container '%s'", container_name)
+        
+        container_id = self.run(f"docker run -d --name {container_name} -p {port}:3000 {self.image}:{self.branch}")
+        
+        
+        self.log.info("Started container with id '%s'", container_id[:12])
+        
+        WAIT_TIME = 5
+        
         self.log.info("Waiting %d seconds to verify container up", WAIT_TIME)
         time.sleep(WAIT_TIME)
+        
+        
         if self.container_is_running(container):
             self.log.info("Container appears to be up")
         else:
@@ -68,35 +109,60 @@ class DeployHelper:
             
         self.docker_ps()
         
-        self.log.info("======= FINISH DEPLOYMENT =======")
+        self.log.info("======= START DEPLOYMENT of '%s' =======", container_name)
             
     def run(self, cmd):
-        return check_output(cmd, shell=True).decode()
+        '''
+        Wrapper function to run a shell command
+        '''
+        return check_output(cmd, shell=True, stderr=subprocess.STDOUT).decode()
         
     def fail(self, reason: str):
+        '''
+        Common utility to bail out of processing for 
+        a given reason.
+        '''
         self.log.fatal(reason)
-        exit(1)
+        sys.exit(1)
         
     def container_is_running(self, container_name) -> bool:
+        '''
+        Uses the word count utility to determine if any lines containing 
+        the container name are found, which indicates if it is running.
+        '''
         running_count = int(self.run(f'docker ps | grep \'{container_name}\' | wc -l'))
         return running_count > 0
 
     def docker_ps(self):
+        '''
+        Wrapper function to perform a `docker ps`
+        command.
+        '''
         self.log.debug("$ docker ps ")
-        self.log.debug("\n" + self.run("docker ps"))
+        self.log.debug("\n%s", self.run("docker ps"))
 
     def get_free_port(self):
+        '''
+        Locates a free port in the correct range for the image 
+        looking to bind to one.
+            3000        -> ecohive-ui:latest    (reserved)
+            3001-3999   -> ecohive-ui:<branch>
+            4000        -> ecohive-api:latest   (reserved)
+            4001-4999   -> ecohive-api:<branch>
+        '''
         
         # If this is a production image, return the standard port
         if self.branch == 'main':
             return self.ports[self.image]
         
-        self.log.info("Dev %s image, getting a free port", self.image)
+        self.log.info("Image %s:%s is development, getting a free port", self.image, self.branch)
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+        # calculate the port range
         min_port = self.ports[self.image] + 1
         max_port = min_port + 999
         
+        # try to bind to each port in this range until we find one that is usable
         for port in range(min_port, max_port):
             try:
                 s.bind(("127.0.0.1", port))
@@ -110,7 +176,7 @@ class DeployHelper:
                     # something else raised the socket.error exception
                     self.fail(e)
         s.close()
-        return usable_port    
+        return usable_port
 
 
 
