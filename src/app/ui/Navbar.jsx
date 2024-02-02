91747841222275bd4b49dc97141462443b62a70e
Toby Keegan <Toby.Keegan@ibm.com>
2024-02-02 09:53:29 +0000

    Use new global variables

--- src/app/ui/Navbar.jsx
+++ src/app/ui/Navbar.jsx
@@ -1,9 +1,9 @@
 import Grid from "@mui/joy/Grid";
-
+import globals from "../global.vars";
 export default function Navbar() {
 	return (
 		<nav id="navbar">
-			<h1>EcoHive Navbar</h1>
+			<h1>{globals.metadata.title}</h1>
 			<a href="/">Home</a>
 			<a href="/settings">Settings</a>
 			<a href="/events/feed">Events</a>
