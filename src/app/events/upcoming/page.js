c64ff2f7102fa313691b3aee7aca6adb549ca999
Toby Keegan <Toby.Keegan@ibm.com>
2024-02-20 09:02:23 +0000

    Sort routing out

--- src/app/events/upcoming/page.js
+++ src/app/events/upcoming/page.js
@@ -1,10 +1,11 @@
 import Navbar from '@/app/ui/Navbar';
-
+import Event from '../_components/Event';
 export default function Feed() {
   return (
     <main>
       <Navbar />
-      <h1> Events feed page template </h1>
+      <h1> Upcoming events feed page template </h1>
+      <Event />
     </main>
   );
 }
