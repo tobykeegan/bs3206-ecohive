2a6c7936f0f520351e3b73926ba9d057b818315f
Toby Keegan <Toby.Keegan@ibm.com>
2024-02-22 10:47:58 +0000

    Ping pong

---
+++ src/app/api/models/ping.model.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose"
+const Schema = mongoose.Schema;
+
+const PingSchema = new Schema({
+  timestamp: Date,
+  ip : String
+});
+
+const Ping = mongoose.models.pings || mongoose.model('Ping', PingSchema);
+
+export default Ping;
--- src/app/api/ping/route.js
+++ src/app/api/ping/route.js
@@ -1,5 +1,25 @@
 import { NextResponse } from 'next/server';
+import Ping from '@/app/api/models/ping.model';
+import { connect } from '../services/mongoose.service'
+
+
+connect();
 
 export async function GET(request) {
-  return NextResponse.json({ message: 'pong' }, { status: 418 });
+  let thisPing = new Ping({ timestamp: new Date(), ip: request.ip })
+
+  let res = {
+    message : 'pong',
+    dbstate : 'connected'
+  }
+  try{
+    await thisPing.save()
+  }catch(err){
+    console.log(err);
+    res.message = 'thud';
+    res.dbstate = 'failed';
+    res.error = err;
+  }
+
+  return NextResponse.json(res, { status: 418 });
 }
