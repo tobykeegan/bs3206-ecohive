0cf42ddf71cf5da1e7b8ead18916804a3d5a06b7
Toby Keegan <Toby.Keegan@ibm.com>
2024-02-22 13:21:57 +0000

    Code tidy up

--- src/.events/history/page.js
+++ src/.events/history/page.js
@@ -1,12 +1,12 @@
 import Navbar from '@/app/ui/Navbar';
-
+import Event from '../_components/Event';
 export default function History() {
   return (
     <main>
       <Navbar />
       <h1> Events history page template </h1>
 
-
+      <Event />
     </main>
   );
 }
--- src/app/events/upcoming/page.js
+++ src/app/events/upcoming/page.js
@@ -1,11 +1,9 @@
 import Navbar from '@/app/ui/Navbar';
-import Event from '../_components/Event';
 export default function Feed() {
   return (
     <main>
       <Navbar />
       <h1> Upcoming events feed page template </h1>
-      <Event />
     </main>
   );
 }
