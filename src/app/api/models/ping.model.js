11a9da195ac671d9383a78d8629a2b95ad575017
Toby Keegan <Toby.Keegan@ibm.com>
2024-02-22 11:06:24 +0000

    Linting

--- src/app/api/models/ping.model.js
+++ src/app/api/models/ping.model.js
@@ -1,9 +1,9 @@
-import mongoose from "mongoose"
+import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const PingSchema = new Schema({
   timestamp: Date,
-  ip : String
+  ip: String,
 });
 
 const Ping = mongoose.models.pings || mongoose.model('Ping', PingSchema);
--- src/app/api/ping/route.js
+++ src/app/api/ping/route.js
@@ -1,20 +1,19 @@
 import { NextResponse } from 'next/server';
 import Ping from '@/app/api/models/ping.model';
-import { connect } from '../services/mongoose.service'
-
+import { connect } from '../services/mongoose.service';
 
 connect();
 
 export async function GET(request) {
-  let thisPing = new Ping({ timestamp: new Date(), ip: request.ip })
+  let thisPing = new Ping({ timestamp: new Date(), ip: request.ip });
 
   let res = {
-    message : 'pong',
-    dbstate : 'connected'
-  }
-  try{
-    await thisPing.save()
-  }catch(err){
+    message: 'pong',
+    dbstate: 'connected',
+  };
+  try {
+    await thisPing.save();
+  } catch (err) {
     console.log(err);
     res.message = 'thud';
     res.dbstate = 'failed';
