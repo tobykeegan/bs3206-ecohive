81b5675857b01e2695e096af2762cdfe05ec5bfa
Toby Keegan <Toby.Keegan@ibm.com>
2024-02-22 13:20:24 +0000

    Add bundle analyser

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+})
+
 const nextConfig = {
   output: 'standalone',
   productionBrowserSourceMaps: false,
   optimizeFonts: false,
 };
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
