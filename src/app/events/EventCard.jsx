25ef4b576f15d957b9214e57587edcecf2b5b9a7
Toby Keegan <Toby.Keegan@ibm.com>
2024-02-29 14:52:30 +0000

    Initial Event card implementation

--- src/app/events/EventCard.jsx
+++ src/app/events/EventCard.jsx
@@ -1,144 +1,89 @@
-'use server';
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import * as React from 'react';
 import AspectRatio from '@mui/joy/AspectRatio';
-import Avatar from '@mui/joy/Avatar';
-import Box from '@mui/joy/Box';
 import Card from '@mui/joy/Card';
 import CardContent from '@mui/joy/CardContent';
 import CardOverflow from '@mui/joy/CardOverflow';
 import Link from '@mui/joy/Link';
-import IconButton from '@mui/joy/IconButton';
-import Input from '@mui/joy/Input';
+import PlaceIcon from '@mui/icons-material/Place';
+import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined';
 import Typography from '@mui/joy/Typography';
-import MoreHoriz from '@mui/icons-material/MoreHoriz';
-import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
-import ModeCommentOutlined from '@mui/icons-material/ModeCommentOutlined';
-import SendOutlined from '@mui/icons-material/SendOutlined';
-import Face from '@mui/icons-material/Face';
-import BookmarkBorderRoundedIcon from '@mui/icons-material/BookmarkBorderRounded';
 import Image from 'next/image';
-import { Chip } from '@mui/joy';
-import TagList from './TagList';
-import trees from '../static/default_event.jpeg';
+import defaultEventImg from '@/static/default_event.jpeg';
+import { Badge, Stack } from 'react-bootstrap';
+import styles from '../styles/events/styles.scss';
+import getRandomColour from '@/utils/colours';
+const getFormattedDate = (convertDate) => {
+  let date = new Date(convertDate);
+  if (date.toString() === 'Invalid Date') return 'N/A';
 
-export default async function EventCard({ ...props }) {
-  const event = props.details;
+  let dateString = date.toLocaleDateString('en-FB', {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+  });
+  return dateString;
+};
 
+const getImageSrc = (event) => {
+  return defaultEventImg;
+};
+
+export default function EventCard({ event }) {
   return (
-    <Card variant="outlined">
+    <Card
+      variant="outlined"
+      sx={{
+        minWidth: 300,
+      }}
+    >
       <CardContent
         orientation="horizontal"
         sx={{ alignItems: 'center', gap: 1 }}
       >
-        <Box
-          sx={{
-            'position': 'relative',
-            '&::before': {
-              content: '""',
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              bottom: 0,
-              right: 0,
-              m: '-2px',
-              borderRadius: '50%',
-              background:
-                'linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%)',
-            },
-          }}
-        ></Box>
-        <Typography fontWeight="lg">{event.name || 'Default Name'}</Typography>
-        <Link
-          component="button"
-          underline="none"
-          fontSize="10px"
-          sx={{ color: 'text.tertiary', my: 0.5 }}
-        >
-          2 DAYS AGO
-        </Link>
+        <Typography fontWeight="lg">{event.name}</Typography>
       </CardContent>
       <CardOverflow>
         <AspectRatio>
           <Image
-            src={trees}
-            alt="Default event image"
-            width="200"
-            height="200"
-          ></Image>
+            src={getImageSrc(event.photoUrl)}
+            width={500}
+            height={500}
+            alt="Picture of the event"
+          />
         </AspectRatio>
       </CardOverflow>
       <CardContent
         orientation="horizontal"
         sx={{ alignItems: 'center', mx: -1 }}
       >
-        <Box sx={{ width: 0, display: 'flex', gap: 0.5 }}>
-          <IconButton variant="plain" color="neutral" size="sm">
-            <FavoriteBorder />
-          </IconButton>
-          <IconButton variant="plain" color="neutral" size="sm">
-            <ModeCommentOutlined />
-          </IconButton>
-          <IconButton variant="plain" color="neutral" size="sm">
-            <SendOutlined />
-          </IconButton>
-        </Box>
-        <Box
-          sx={{ display: 'flex', alignItems: 'center', gap: 0.5, mx: 'auto' }}
-        ></Box>
-        <Box sx={{ width: 0, display: 'flex', flexDirection: 'row-reverse' }}>
-          <IconButton variant="plain" color="neutral" size="sm">
-            <BookmarkBorderRoundedIcon />
-          </IconButton>
-        </Box>
+        <Stack gap={3} direction="horizontal">
+          <Badge className="eventChip" id="location-chip" pill>
+            <PlaceIcon />
+            {event.location}
+          </Badge>
+          <Badge className="eventChip" id="date-chip" pill>
+            <CalendarMonthOutlinedIcon />
+            {getFormattedDate(event.date)}
+          </Badge>
+        </Stack>
       </CardContent>
       <CardContent>
         <Link
           component="button"
           underline="none"
-          fontSize="sm"
-          fontWeight="lg"
-          textColor="text.primary"
-        >
-          1305 sign ups
-        </Link>
-        <Typography fontSize="sm">{event.description}</Typography>
-        <Link
-          component="button"
-          underline="none"
-          fontSize="sm"
-          startDecorator="…"
-          sx={{ color: 'text.tertiary' }}
-        >
-          more
-        </Link>
-
-        <TagList
-          tags={[
-            {
-              name: 'Tag 1',
-              colour: 'primary',
-            },
-            {
-              name: 'Tag 2',
-              colour: 'secondary',
-            },
-          ]}
-        />
-      </CardContent>
-      <CardContent orientation="horizontal" sx={{ gap: 1 }}>
-        <IconButton size="sm" variant="plain" color="neutral" sx={{ ml: -1 }}>
-          <Face />
-        </IconButton>
-        <Input
-          variant="plain"
-          size="sm"
-          placeholder="Add a comment…"
-          sx={{ 'flex': 1, 'px': 0, '--Input-focusedThickness': '0px' }}
-        />
-        <Link disabled underline="none" role="button">
-          Post
-        </Link>
+          fontSize="10px"
+          sx={{ color: 'text.tertiary', my: 0.5 }}
+        ></Link>
+        <Typography fontSize="sm">
+          <Link
+            component="button"
+            color="neutral"
+            fontWeight="lg"
+            textColor="text.primary"
+          ></Link>{' '}
+          {event.description}
+        </Typography>
       </CardContent>
     </Card>
   );
--- src/app/events/discover/page.js
+++ src/app/events/discover/page.js
@@ -1,17 +1,27 @@
-'use server';
 import Navbar from '@/app/ui/Navbar';
 import EventCard from '../EventCard';
-import Event from '@/app/api/models/event.model';
+import Container from 'react-bootstrap/Container';
 import Divider from '@mui/joy/Divider';
 import Footer from '@/app/ui/Footer';
-
+import axios from 'axios';
+import { URL } from '@/utils/globals';
+import style from '../../styles/events/styles.scss';
 export default async function Discover() {
+  let eventCards;
+  try {
+    let res = await axios.get(`${URL}/api/events/discover`);
+    eventCards = res.data.map((event) => {
+      return <EventCard key={event._id} event={event} />;
+    });
+  } catch (err) {
+    console.log(err);
+  }
+
   return (
     <main>
       <Navbar />
-      <h1> Discover events feed page template </h1>
+      <div id="eventsContainer">{eventCards}</div>
 
-      {/* <EventCard/> */}
       <Divider />
       <Footer />
     </main>
