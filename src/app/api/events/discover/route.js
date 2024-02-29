3c41c96b5600f2db7fae6925d5b61e0edc832e33
Toby Keegan <Toby.Keegan@ibm.com>
2024-02-29 14:51:36 +0000

    GET on /events/discover returns all events

--- src/app/api/events/discover/route.js
+++ src/app/api/events/discover/route.js
@@ -1,8 +1,21 @@
 import { NextResponse } from 'next/server';
+import Event from '@/models/event';
 import { connect } from '@/services/mongoose';
 
 await connect();
 
-export async function GET(request) {}
+/**
+ *
+ * @param {*} request
+ */
+export async function GET(request) {
+  // Form data will be used to modify the request
+  //   const formData = await request.formData();
+  // until the form is hooked in, just return all the events
+
+  let eventsList = await Event.find({});
+
+  return NextResponse.json(eventsList);
+}
 
 export async function POST(request) {}
