0574eeb5c2c7f80f9b6bf583ad4312ff73dd04a3
Toby Keegan <Toby.Keegan@ibm.com>
2024-03-11 09:41:59 +0000

    Add creator info to events, rename photo URL

--- src/app/api/models/event.model.js
+++ src/app/api/models/event.model.js
@@ -6,11 +6,15 @@ const EventSchema = new Schema({
   type: String,
   location: String,
   description: String,
+  creator: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
   attendance: {
     capacity: Number,
     signups: Number,
   },
-  photoUrl: String,
+  photoBase64: String,
   points: Number,
   tags: [
     {
