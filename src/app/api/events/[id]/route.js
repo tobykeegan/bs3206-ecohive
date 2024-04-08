dce941082c12f34ba62202d6a660e5d6d3f32799
Toby Keegan <Toby.Keegan@ibm.com>
2024-04-08 12:02:39 +0100

    Add PUT function to API route

--- src/app/api/events/[id]/route.js
+++ src/app/api/events/[id]/route.js
@@ -2,10 +2,16 @@ import { NextResponse } from 'next/server';
 import Event from '@/models/event';
 
 import handleGet from './GET';
+import handlePut from './PUT';
 import handleDelete from './DELETE';
 
 // Handles the GET request for retrieving an event by ID.
 export async function GET(request, { params }) {
+  return handleGet(params.id);
+}
+
+// Handles the PUT request for retrieving an event by ID.
+export async function PUT(request, { params }) {
   return handleGet(params.id);
 }
 
