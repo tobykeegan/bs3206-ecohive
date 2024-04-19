1aa71733d2ae09d0fdc48904a5c1c1c2cfd80c98
Toby Keegan <Toby.Keegan@ibm.com>
2024-04-19 12:00:04 +0100

    Playwright please just use the container I'm telling you to before I cry

--- playwright.config.js
+++ playwright.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   webServer: {
     command: 'bun run start',
     url: 'http://0.0.0.0:3000',
-    reuseExistingServer: Boolean(process.env.CI),
+    reuseExistingServer: true,
     timeout: (parseInt(process.env.WEB_TIMEOUT) || 360) * 1000,
   },
   testDir: 'tests',
