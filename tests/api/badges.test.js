8c0d5a79f88be0364076b4dc37343883a5f6c6d3
Jade Carino <carino_jade@yahoo.co.uk>
2024-04-25 16:19:55 +0100

    Improvement to Badges GET test
    Signed-off-by: Jade Carino <carino_jade@yahoo.co.uk>

--- tests/api/badges.test.js
+++ tests/api/badges.test.js
@@ -11,18 +11,26 @@ test.describe('GET request to /api/badges', () => {
   test('Badge objects are returned', async ({ request }) => {
     const response = await request.get('/api/badges');
     const body = await response.json();
-    const bodyString = JSON.stringify(body);
-
     expect(response.status()).toBe(HTTP.OK);
-    expect(bodyString).toContain('badges');
-    expect(bodyString).toContain('__v');
-    expect(bodyString).toContain('_id');
-    expect(bodyString).toContain('id');
-    expect(bodyString).toContain('name');
-    expect(bodyString).toContain('description');
-    expect(bodyString).toContain('imageId');
-    expect(bodyString).toContain('type');
-    expect(bodyString).toContain('criteria');
-    expect(bodyString).toContain('serializedFunction');
+
+    expect(body).toBeInstanceOf(Object);
+    expect(body).toHaveProperty('badges');
+    expect(body).toHaveProperty('status', 200);
+
+    const badges = body.badges;
+    expect(badges).toBeInstanceOf(Array);
+
+    badges.forEach((badge) => {
+      expect(badge).toBeInstanceOf(Object);
+      expect(badge).toHaveProperty('id');
+      expect(badge).toHaveProperty('name');
+      expect(badge).toHaveProperty('description');
+      expect(badge).toHaveProperty('imageId');
+      expect(badge).toHaveProperty('type');
+
+      expect(badge).toHaveProperty('criteria');
+      const criteria = badge.criteria;
+      expect(criteria).toHaveProperty('serializedFunction');
+    });
   });
 });
