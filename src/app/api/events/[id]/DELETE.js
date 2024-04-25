764b9d18f3faa2e7d5a405e48bb6e423176a1520
Toby Keegan <Toby.Keegan@ibm.com>
2024-04-25 14:02:35 +0100

    Add author tag

--- src/app/api/events/[id]/DELETE.js
+++ src/app/api/events/[id]/DELETE.js
@@ -2,6 +2,13 @@ import Event from '@/models/event';
 import { NextResponse } from 'next/server';
 import logger from '@/utils/logger';
 import { HTTP } from '@/utils/globals';
+
+/**
+ * Delete an event in the database by ID
+ *
+ * @param {string} id - the ID of the event to delete
+ * @author Toby Keegan
+ */
 export default async function deleteEvent(id) {
   logger.info('Delete Event: ', id);
   let res = await Event.findByIdAndDelete(id);
