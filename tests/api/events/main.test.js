f9e29906c21c7a2edd471677f3df4d4a8ffb83fb
Toby Keegan <Toby.Keegan@ibm.com>
2024-05-01 18:45:02 +0100

    Migrate deletion tests to main file

--- tests/api/events/main.test.js
+++ tests/api/events/main.test.js
@@ -2,6 +2,7 @@ import { expect, test } from '@playwright/test';
 import { mongo, HTTP, URL } from '@/utils/globals';
 import { connect } from '@/services/mongoose';
 import Event from '@/models/event';
+import Attendance from '@/models/attendance';
 import { generateEvent, randomString } from '@/test/utils';
 require('dotenv').config();
 
@@ -47,112 +48,221 @@ test.describe('Events API operations', () => {
     );
   });
 
-  test.beforeEach(async ({ request }) => {});
   test.afterAll(async () => {
-    // Delete all the events that were created for this test
+    // collect the IDs of every event that was created for this test
+    const trackedEvents = await Event.find({ description: TRACKED });
+
+    // for each event, delete the attendance records
+    trackedEvents.forEach(async (event) => {
+      await Attendance.deleteMany({ event: event._id });
+    });
+
+    // now finally delete all those events
     await Event.deleteMany({ description: TRACKED });
   });
+  test.describe('Read Operations', () => {
+    test('GET /events returns a list of event type objects', async ({
+      request,
+    }) => {
+      const response = await request.get('/api/events');
+      const body = await response.json();
+      expect(response.status()).toBe(HTTP.OK);
 
-  test('GET /events returns a list of event type objects', async ({
-    request,
-  }) => {
-    const response = await request.get('/api/events');
-    const body = await response.json();
-    expect(response.status()).toBe(HTTP.OK);
+      // Check that the response body is an array
+      expect(body).toBeInstanceOf(Array);
 
-    // Check that the response body is an array
-    expect(body).toBeInstanceOf(Array);
-
-    // Check that each element of the array is a vaguely correct Event object
-    body.forEach((event) => {
-      expect(event).toBeInstanceOf(Object);
-      expect(event).toHaveProperty('name');
-      expect(event).toHaveProperty('date');
-      expect(event).toHaveProperty('location');
-      expect(event).toHaveProperty('description');
-      expect(event).toHaveProperty('creator');
+      // Check that each element of the array is a vaguely correct Event object
+      body.forEach((event) => {
+        expect(event).toBeInstanceOf(Object);
+        expect(event).toHaveProperty('name');
+        expect(event).toHaveProperty('date');
+        expect(event).toHaveProperty('location');
+        expect(event).toHaveProperty('description');
+        expect(event).toHaveProperty('creator');
+      });
     });
-  });
-  test('POST /events returns all events if a blank body is provided', async ({
-    request,
-  }) => {
-    const eventCount = await Event.countDocuments();
-    // add an empty json object to the request body
-    const response = await request.post('/api/events', {
-      data: {},
+    test('POST /events returns all events if a blank body is provided', async ({
+      request,
+    }) => {
+      const eventCount = await Event.countDocuments();
+      // add an empty json object to the request body
+      const response = await request.post('/api/events', {
+        data: {},
+      });
+
+      const body = await response.json();
+
+      expect(response.status()).toBe(HTTP.OK);
+      expect(body).toBeInstanceOf(Array);
+      expect(body.length).toBe(eventCount);
+      // expect the length of the array to be the same as the number of events
     });
+    test('GET /events/:id returns a single event', async ({ request }) => {
+      // Grab any event from the database - we don't care which one
+      const event = await Event.findOne();
 
-    const body = await response.json();
+      // check event is not null
+      expect(event).not.toBeNull();
 
-    expect(response.status()).toBe(HTTP.OK);
-    expect(body).toBeInstanceOf(Array);
-    expect(body.length).toBe(eventCount);
-    // expect the length of the array to be the same as the number of events
-  });
-  test('GET /events/:id returns a single event', async ({ request }) => {
-    // Grab any event from the database - we don't care which one
-    const event = await Event.findOne();
+      // Make a request to the API to get the event by ID
+      const response = await request.get(`/api/events/${event._id}`);
+      const body = await response.json();
 
-    // check event is not null
-    expect(event).not.toBeNull();
+      expect(response.status()).toBe(HTTP.OK);
 
-    // Make a request to the API to get the event by ID
-    const response = await request.get(`/api/events/${event._id}`);
-    const body = await response.json();
-
-    expect(response.status()).toBe(HTTP.OK);
-
-    // Check that the response body is the same as the event we grabbed
-    expect(JSON.stringify(body)).toEqual(JSON.stringify(event));
-  });
-
-  test('GET /events/:id returns 404 if no event is found', async ({
-    request,
-  }) => {
-    // Make a request to the API to get an event that we know doesn't exist
-    const response = await request.get('/api/events/12a9db7cc7de36df18301234');
-    const body = await response.json();
-    expect(response.status()).toBe(HTTP.NOT_FOUND);
-    expect(body).toHaveProperty('message');
-    expect(body.message).toBe('No event found matching:');
-  });
-
-  // Test that all the events matching this description are returned
-  test('POST /events returns multiple events by specifying a search criteria', async ({
-    request,
-  }) => {
-    // Count the number of tracked events in the database
-    const trackedEvents = await Event.countDocuments({ description: TRACKED });
-
-    // now query the API for our tracked events
-    const response = await request.post('/api/events', {
-      data: JSON.stringify({ description: TRACKED }),
+      // Check that the response body is the same as the event we grabbed
+      expect(JSON.stringify(body)).toEqual(JSON.stringify(event));
     });
 
-    const body = await response.json();
-    expect(response.status()).toBe(HTTP.OK);
-    expect(body).toBeInstanceOf(Array);
-    expect(body.length).toBe(trackedEvents);
-    body.forEach((event) => {
-      expect(event).toHaveProperty('description');
-      expect(event.description).toEqual(TRACKED);
+    test('GET /events/:id returns 404 if no event is found', async ({
+      request,
+    }) => {
+      // Make a request to the API to get an event that we know doesn't exist
+      const response = await request.get(
+        '/api/events/12a9db7cc7de36df18301234',
+      );
+      const body = await response.json();
+      expect(response.status()).toBe(HTTP.NOT_FOUND);
+      expect(body).toHaveProperty('message');
+      expect(body.message).toBe('No event found matching:');
+    });
+
+    // Test that all the events matching this description are returned
+    test('POST /events returns multiple events by specifying a search criteria', async ({
+      request,
+    }) => {
+      // Count the number of tracked events in the database
+      const trackedEvents = await Event.countDocuments({
+        description: TRACKED,
+      });
+
+      // now query the API for our tracked events
+      const response = await request.post('/api/events', {
+        data: JSON.stringify({ description: TRACKED }),
+      });
+
+      const body = await response.json();
+      expect(response.status()).toBe(HTTP.OK);
+      expect(body).toBeInstanceOf(Array);
+      expect(body.length).toBe(trackedEvents);
+      body.forEach((event) => {
+        expect(event).toHaveProperty('description');
+        expect(event.description).toEqual(TRACKED);
+      });
     });
   });
-  // Test that an event can be updated
-  test('PUT /events/:id updates an event', async ({ request }) => {
-    // Get a single event from the database
-    const event = await Event.findOne();
 
-    // check event is not null
-    expect(event).not.toBeNull();
+  test.describe('Write Operations', () => {
+    test('POST /events/new creates a new event', async ({ request }) => {
+      let req = await request.get('/api/whoami');
+      let user = await req.json();
+      // Create a new event
+      const event = generateEvent(user.id, true);
+      // Save the event to the database
+      const response = await request.post('/api/events/new', {
+        data: JSON.stringify(event),
+      });
+      const body = await response.json();
+      expect(response.status()).toBe(HTTP.CREATED);
+      expect(body).toHaveProperty('name');
+      expect(body.name).toBe(event.name);
 
-    // update the event
-    const response = await request.put(`/api/events/${event._id}`, {
-      data: { name: `Updated Event Name for ${TRACKED}` },
+      // Tidy up and delete the event
+      await Event.deleteOne({ _id: body._id });
+
+      // Check the event was deleted
+      const deletedEvent = await Event.find({ _id: body._id });
+      expect(deletedEvent).toBeInstanceOf(Array);
+      expect(deletedEvent.length).toBe(0);
     });
-    const body = await response.json();
-    expect(response.status()).toBe(HTTP.CREATED);
-    expect(body).toHaveProperty('name');
-    expect(body.name).toBe(`Updated Event Name for ${TRACKED}`);
+    test('PUT /events/:id updates an event', async ({ request }) => {
+      // Get a single event from the database
+      const event = await Event.findOne();
+
+      // check event is not null
+      expect(event).not.toBeNull();
+
+      // update the event
+      const response = await request.put(`/api/events/${event._id}`, {
+        data: { name: `Updated Event Name for ${TRACKED}` },
+      });
+      const body = await response.json();
+      expect(response.status()).toBe(HTTP.CREATED);
+      expect(body).toHaveProperty('name');
+      expect(body.name).toBe(`Updated Event Name for ${TRACKED}`);
+    });
+  });
+
+  test.describe('Delete Operations', () => {
+    // test that all attendance records are deleted when an event is deleted
+    test('should delete all attendance records when an event is deleted', async ({
+      request,
+    }) => {
+      let req = await request.get('/api/whoami');
+      let user = await req.json();
+
+      // Create a new event
+      const event = generateEvent(user.id);
+      // Save the event to the database
+      await event.save();
+
+      const eventId = event._id.toString();
+
+      for (let i = 0; i < 10; i++) {
+        await new Attendance({
+          event: eventId,
+          user: user.id,
+        }).save();
+      }
+
+      // now delete the event
+      const response = await request.delete(`/api/events/${eventId}`);
+      const body = await response.json();
+
+      await expect(response.status()).toBe(HTTP.OK);
+      await expect(body).toHaveProperty('message');
+      await expect(body.message).toEqual('Event deleted successfully');
+
+      // now check there are no attendance records left
+      const recordAfterDelete = await Attendance.findOne({ event: eventId });
+      console.log(recordAfterDelete);
+      await expect(recordAfterDelete).toBeNull();
+    });
+    test('DELETE /events/:id deletes an event', async ({ request }) => {
+      let req = await request.get('/api/whoami');
+      let user = await req.json();
+      // Create a new event
+      const event = generateEvent(user.id);
+      // Save the event to the database
+      await event.save();
+      const thisId = event._id.toString();
+      // check if the event is saved
+      let foundEvent = await Event.findById(thisId);
+      await expect(foundEvent).not.toBeNull();
+
+      // Store the event ID for later
+      const response = await request.delete(`/api/events/${thisId}`);
+      const body = await response.json();
+      await expect(response.status()).toBe(HTTP.OK);
+      await expect(body).toHaveProperty('message');
+      await expect(body.message).toEqual('Event deleted successfully');
+      await expect(body).toHaveProperty('id');
+      await expect(body.id).toEqual(thisId);
+    });
+
+    test('DELETE /events/:id returns 404 if no event is found', async ({
+      request,
+    }) => {
+      // Make a request to the API to delete an event that we know doesn't exist
+      const response = await request.delete(
+        '/api/events/12a9db7cc7de36df18301234',
+      );
+      const body = await response.json();
+      expect(response.status()).toBe(HTTP.NOT_FOUND);
+      expect(body).toHaveProperty('message');
+      expect(body.message).toBe(
+        'No event found with ID: 12a9db7cc7de36df18301234',
+      );
+    });
   });
 });
