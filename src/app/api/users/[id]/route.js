992e9b98d1c0dd0bbf1d2081aac75ccf21a282c7
Toby Keegan <Toby.Keegan@ibm.com>
2024-05-08 08:24:05 +0100

    API endpoint to get some interesting stuff about users

---
+++ src/app/api/users/[id]/route.js
@@ -0,0 +1,42 @@
+import User from '@/models/user';
+import { NextResponse } from 'next/server';
+import { HTTP } from '@/utils/globals';
+import { ConstructionOutlined } from '@mui/icons-material';
+
+export async function GET(request, { params }) {
+  let user;
+
+  const id = params.id;
+  try {
+    console.log('Finding user with ID: ', id);
+    user = await User.findById(id);
+    console.log('Found user: ', user);
+  } catch (err) {
+    return NextResponse.json(
+      {
+        message: 'Error finding user',
+        error: err,
+      },
+      {
+        status: HTTP.BAD_REQUEST,
+      },
+    );
+  }
+
+  if (!user) {
+    return NextResponse.json(
+      {
+        message: 'No user found matching:',
+        search: { _id: id },
+      },
+      {
+        status: HTTP.NOT_FOUND,
+      },
+    );
+  }
+  // Return the event if found
+  return NextResponse.json({
+    name: `${user.name.first} ${user.name.last}`,
+    displayName: user.displayName,
+  });
+}
